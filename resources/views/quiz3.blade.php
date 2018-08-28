98dbf6bcdbe608e5952f6831fec5cf2a09abe7cd
l3eeR <beerb28@gmail.com>
2018-08-28 12:36:28 +0700

    up3

---
+++ resources/views/quiz3.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body><img class="sy-logo" src="{{url('/')}}/css/pubg/sy_logo.png">
+<a><img class="sy-logo" src="{{url('/')}}/css/pubg/pubg-mobile_logo.png"></a>
+<div class="sy-navi">
+            <a class="on" data-pos="0" data-index="0">Home Page</a>
+            <a data-pos="202" data-index="2" href="//groups.google.com/forum/#!forum/tencentgamingbuddy" data-stats="sysem.ak.to_wjlt" target="_blank">Player Forum</a>
+        </div>
+        
+</body>
+</html>
